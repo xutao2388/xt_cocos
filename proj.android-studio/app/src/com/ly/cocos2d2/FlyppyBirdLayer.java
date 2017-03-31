f8a58a81ab2111084a729900055375fdba67a9ea
xutao0@plu.cn <xutao0@plu.cn>
2017-03-31 18:32:27 +0800

    [add] add annotation

--- proj.android-studio/app/src/com/ly/cocos2d2/FlyppyBirdLayer.java
+++ proj.android-studio/app/src/com/ly/cocos2d2/FlyppyBirdLayer.java
@@ -31,8 +31,8 @@ import java.util.Random;
 public class FlyppyBirdLayer extends CCLayer implements ContactListener {
     protected static final float PTM_RATIO = 32.0f;
     protected static final float FPS = (float) CCDirector.sharedDirector().getAnimationInterval();//Frames Per Second,每秒传输帧数
-    private World world;//世界，第三方库libGDX，
-    private CCSprite bird;
+    private World world;//世界，第三方库libGDX，== initWorld
+    private CCSprite bird;//游戏背景、NPC、人物、道具等。在cocos2d-x引擎中，只要是用图片展示的，基本上需要使用精灵类。== addBird
     private CGSize screenSize;
     private Context mContext;
     private int score;
@@ -47,21 +47,69 @@ public class FlyppyBirdLayer extends CCLayer implements ContactListener {
         startGame();
     }
 
+    //    首先设置当前场景的物理环境，这里我们使用Box2D，=libs中的box2d.jar；
+    private void initWorld() {
+        Vector2 gravity = new Vector2(0f, -9.8f);//向量的X组件。向量的Y组件。
+        world = new World(gravity, true);//创建世界
+        world.setContinuousPhysics(true);//允许物理现象
+        world.setContactListener(this);//设置接触监听
+    }
+
+    private void startGame() {
+        score = 0;
+        addBird();
+        addGround();
+        scheduleUpdate();//需要每次重绘小鸟位置，否则小鸟表现为不会运动，我们调用scheduleUpdate()方法，来执行默认的update(float dt)方法（Cocos2d机制）==可理解为更新函数，
+        // 对应的是:// unscheduleUpdate()=停止更新；
+        schedule("addBar", 2);//参数1是目标更新函数（对应addBar方法）,参数2的单位是秒；即每隔2秒更新一次addBar函数；
+        // schedule的作用与scheduleUpdate()函数相同，但是scheduleUpdate()默认每一帧都会调用update函数，而schedule则可以自定义调用更新函数的时间间隔。
+//  <1>     schedule(SEL_SCHEDULE selector,float interval)参数1： 目标更新函数。参数2： 刷新间隔。
+//  <2>     schedule(SEL_SCHEDULE selector)作用： 此函数默认每一帧都调用目标函数。 参数： 目标更新函数。注意： 刷新间隔时间的单位是s（秒）。
+//          unschedule(SEL_SCHEDULE selector);停止自定义更新函数。
+//          unscheduleAllSelectors() 停止所有更新函数。
+    }
+
+    //添加小鸟
+    private void addBird() {
+        bird = CCSprite.sprite("bird.png");//创建精灵
+        CGSize s = bird.getContentSize();//获取图片尺寸
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyType.DynamicBody;
+        bodyDef.position.set(300 / PTM_RATIO, 800 / PTM_RATIO);
+
+        PolygonShape dynamicBox = new PolygonShape(); // Define another box shape for our dynamic body.
+        dynamicBox.setAsBox(s.width / 2 / PTM_RATIO, s.height / 2 / PTM_RATIO);
+
+        Body body = world.createBody(bodyDef); // Define the dynamic body fixture and set mass so it's dynamic. 创建body
+        body.setUserData(bird);
+
+
+        FixtureDef fixtureDef = new FixtureDef();//        设置材料
+
+//        fixtureDef.SetAsBox(3,3);//将多边形设为矩形
+        fixtureDef.shape = dynamicBox;//定形
+        fixtureDef.density = 1.0f;//密度
+        fixtureDef.friction = 0.3f;//定义摩擦力
+
+        body.createFixture(fixtureDef);//将已经定形的材料加进body中= 给物体定义形状以及物理材质
+        addChild(bird);
+    }
+
     //    添加滑块，并且设置速度，这里我们仍然使用schedule方法，来隔一段时间添加滑块，并且设置从右往左运动。 这里添加了上下2部分滑块，中间留出空隙，让小鸟可以通过。
+//    被schedule调用
     public void addBar(float dt) {
         float offset = -(new Random().nextInt(5));
         CCSprite downBar = CCSprite.sprite("downBar.png");
         CGSize downBarSize = downBar.getContentSize();
         BodyDef def = new BodyDef();
         def.type = BodyType.KinematicBody;
-        def.position.set(screenSize.width / PTM_RATIO + 2, downBarSize.height
-                / PTM_RATIO + offset);
+        def.position.set(screenSize.width / PTM_RATIO + 2, downBarSize.height / PTM_RATIO + offset);
         def.linearVelocity.set(-5, 0);
 
         Body downBarBody = world.createBody(def);
         PolygonShape downPolygonShape = new PolygonShape();
-        downPolygonShape.setAsBox(downBarSize.width / 2 / PTM_RATIO,
-                downBarSize.height / 2 / PTM_RATIO);
+        downPolygonShape.setAsBox(downBarSize.width / 2 / PTM_RATIO, downBarSize.height / 2 / PTM_RATIO);
         FixtureDef downBarFixtureDef = new FixtureDef();
         downBarFixtureDef.shape = downPolygonShape;
         downBarBody.createFixture(downBarFixtureDef);
@@ -72,15 +120,12 @@ public class FlyppyBirdLayer extends CCLayer implements ContactListener {
         CGSize upBarSize = upBar.getContentSize();
         BodyDef upBardef = new BodyDef();
         upBardef.type = BodyType.KinematicBody;
-        upBardef.position.set(screenSize.width / PTM_RATIO + 2,
-                upBarSize.height / PTM_RATIO + offset + upBarSize.height * 2
-                        / PTM_RATIO);
+        upBardef.position.set(screenSize.width / PTM_RATIO + 2, upBarSize.height / PTM_RATIO + offset + upBarSize.height * 2 / PTM_RATIO);
         upBardef.linearVelocity.set(-5, 0);
 
         Body upBarBody = world.createBody(upBardef);
         PolygonShape upPolygonShape = new PolygonShape();
-        upPolygonShape.setAsBox(upBarSize.width / 2 / PTM_RATIO,
-                upBarSize.height / 2 / PTM_RATIO);
+        upPolygonShape.setAsBox(upBarSize.width / 2 / PTM_RATIO, upBarSize.height / 2 / PTM_RATIO);
         FixtureDef upBarFixtureDef = new FixtureDef();
         upBarFixtureDef.shape = upPolygonShape;
         upBarBody.createFixture(upBarFixtureDef);
@@ -95,9 +140,8 @@ public class FlyppyBirdLayer extends CCLayer implements ContactListener {
         while (it.hasNext()) {
             Body b = it.next();
             Object userData = b.getUserData();
-            if (userData != null && userData instanceof CCSprite
-                    && userData == bird) {
-                b.setLinearVelocity(0, 10);
+            if (userData != null && userData instanceof CCSprite && userData == bird) {
+                b.setLinearVelocity(0, 10);//添加的速度会覆盖刚体原有的速度,x轴不变，Y轴+10
             }
         }
         return super.ccTouchesBegan(event);
@@ -123,52 +167,15 @@ public class FlyppyBirdLayer extends CCLayer implements ContactListener {
         addChild(ground);
     }
 
-    //    改变小鸟速度（在这里设置当点击屏幕时小鸟向上移动）,需要首先设置当前场景的物理环境，这里我们使用Box2D，=libs中的box2d.jar；
-    private void initWorld() {
-        Vector2 gravity = new Vector2(0f, -9.8f);//向量的X组件。向量的Y组件。
-        world = new World(gravity, true);//创建世界
-        world.setContinuousPhysics(true);//允许物理现象
-        world.setContactListener(this);//设置接触监听
-    }
-
-    //添加小鸟
-    private void addBird() {
-        bird = CCSprite.sprite("bird.png");
-        CGSize s = bird.getContentSize();
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyType.DynamicBody;
-        bodyDef.position.set(300 / PTM_RATIO, 800 / PTM_RATIO);
-
-        // Define another box shape for our dynamic body.
-        PolygonShape dynamicBox = new PolygonShape();
-        dynamicBox.setAsBox(s.width / 2 / PTM_RATIO, s.height / 2 / PTM_RATIO);
-        // These are mid points for our 1m box
-        // dynamicBox.density = 1.0f;
-        // dynamicBox.friction = 0.3f;
-
-        // Define the dynamic body fixture and set mass so it's dynamic.
-        Body body = world.createBody(bodyDef);
-        body.setUserData(bird);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = dynamicBox;
-        fixtureDef.density = 1.0f;
-        fixtureDef.friction = 0.3f;
-        body.createFixture(fixtureDef);
-        addChild(bird);
-    }
-
     public void update(float dt) {
         System.out.println(world.getBodyCount());
         world.step(FPS, 8, 1);
-        // Iterate over the bodies in the physics world
-        Iterator<Body> it = world.getBodies();
+        Iterator<Body> it = world.getBodies(); // Iterate over the bodies in the physics world
         while (it.hasNext()) {
             Body b = it.next();
             Object userData = b.getUserData();
             if (userData != null && userData instanceof CCSprite) {
-                // Synchronize the Sprites position and rotation with the
-                // corresponding body
+                // Synchronize the Sprites position and rotation with the corresponding body
                 final CCSprite sprite = (CCSprite) userData;
                 final Vector2 pos = b.getPosition();
                 if (pos.x < 0) {
@@ -180,15 +187,6 @@ public class FlyppyBirdLayer extends CCLayer implements ContactListener {
                 }
             }
         }
-    }
-
-    //    这里需要注意一点，需要每次重绘小鸟位置，否则小鸟表现为不会运动，我们调用scheduleUpdate()方法，来执行默认的update(float dt)方法（Cocos2d机制）
-    private void startGame() {
-        score = 0;
-        addBird();
-        addGround();
-        scheduleUpdate();
-        schedule("addBar", 2);
     }
 
     private void stopGame() {
@@ -237,8 +235,7 @@ public class FlyppyBirdLayer extends CCLayer implements ContactListener {
     //             最后，我们需要碰撞检测
     @Override
     public void beginContact(Contact contact) {
-        if (contact.getFixtureA().getBody().getUserData() == bird
-                || contact.getFixtureB().getBody().getUserData() == bird) {
+        if (contact.getFixtureA().getBody().getUserData() == bird || contact.getFixtureB().getBody().getUserData() == bird) {
             stopGame();
             handler.sendEmptyMessage(0);
         }
--- proj.android-studio/app/src/com/ly/cocos2d2/GameLayer.java
+++ proj.android-studio/app/src/com/ly/cocos2d2/GameLayer.java
@@ -1,160 +1,178 @@
 package com.ly.cocos2d2;
 
+import android.util.Log;
+import android.view.MotionEvent;
+
 import org.cocos2d.actions.base.CCRepeatForever;
 import org.cocos2d.actions.instant.CCCallFuncN;
 import org.cocos2d.actions.interval.CCFadeIn;
 import org.cocos2d.actions.interval.CCFadeOut;
 import org.cocos2d.actions.interval.CCMoveTo;
-import org.cocos2d.actions.interval.CCRepeat;
 import org.cocos2d.actions.interval.CCRotateTo;
 import org.cocos2d.actions.interval.CCScaleTo;
 import org.cocos2d.actions.interval.CCSequence;
 import org.cocos2d.actions.interval.CCTintBy;
-import org.cocos2d.actions.interval.CCTintTo;
 import org.cocos2d.layers.CCLayer;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCSprite;
 import org.cocos2d.types.CGPoint;
 import org.cocos2d.types.ccColor3B;
 
-import android.view.MotionEvent;
+public class GameLayer extends CCLayer {
+    private static final String TAG = "GameLayer";
+    private CCSprite player1, player2;
 
-public class GameLayer extends CCLayer{
-	CCSprite player1,player2;
-	public GameLayer() {
-		cocosd10();
-	}
-	private void cocosd10() {
-		player1 = CCSprite.sprite("plane.png");
-		CGPoint initPos = CGPoint.ccp(100, 100);
-		player1.setPosition(initPos);
-		this.addChild(player1);
-	}
-	private void cocosd9() {
-		this.schedule("func", 1);
-	}
-	
-	public void func(float delta) {
-		System.out.println(delta);
-	}
-	
-	private void cocosd8() {
-		this.setIsTouchEnabled(true);
-	}
-	@Override
-	public boolean ccTouchesBegan(MotionEvent event) {
-		float x = event.getX();
-		float y = event.getY();
-		CGPoint p1 = CGPoint.ccp(x, y);
-		CCDirector director = CCDirector.sharedDirector();
-		CGPoint p2 = director.convertToGL(p1);
-		player1 = CCSprite.sprite("player.png");
-		player1.setPosition(p2);
-		this.addChild(player1);
-		
-		return super.ccTouchesBegan(event);
-	}
-	@Override
-	public boolean ccTouchesEnded(MotionEvent event) {
-		this.removeChild(player1, true);
-		return super.ccTouchesEnded(event);
-	}
-	@Override
-	public boolean ccTouchesMoved(MotionEvent event) {
-		float x = event.getX();
-		float y = event.getY();
-		CGPoint p1 = CGPoint.ccp(x, y);
-		CCDirector director = CCDirector.sharedDirector();
-		CGPoint p2 = director.convertToGL(p1);
-		player1.setPosition(p2);
-		return super.ccTouchesMoved(event);
-	}
-	private void cocosd7() {
-		player1 = CCSprite.sprite("player.png");
-		CGPoint initPos = CGPoint.ccp(100, 100);
-		player1.setPosition(initPos);
-		this.addChild(player1);
-		CGPoint tPos = CGPoint.ccp(300, 300);
-		CCMoveTo moveTo1 = CCMoveTo.action(2, tPos);
-		CCMoveTo moveTo2 = CCMoveTo.action(2, initPos);
-		CCSequence seq = CCSequence.actions(moveTo1, moveTo2);
-		CCRepeatForever repeat = CCRepeatForever.action(seq);
-		player1.runAction(repeat);
-		
-	}
-	private void cocosd6() {
-		player1 = CCSprite.sprite("player.png");
-		CGPoint initPos = CGPoint.ccp(100, 100);
-		player1.setPosition(initPos);
-		this.addChild(player1);
-		ccColor3B cc = ccColor3B.ccc3(-120, 0, -255);
-		CCTintBy tintby = CCTintBy.action(3, cc);
-		player1.runAction(tintby);
-	}
-	private void cocosd5() {
-		player1 = CCSprite.sprite("player.png");
-		CGPoint initPos = CGPoint.ccp(100, 100);
-		player1.setPosition(initPos);
-		this.addChild(player1);
+    public GameLayer() {
+        cocosd2();
+    }
 
-		CCFadeIn fadeIn = CCFadeIn.action(3);
-		CCFadeOut fadeOut = CCFadeOut.action(3);
-		CCSequence seq = CCSequence.actions(fadeIn,fadeOut);
-		player1.runAction(seq);
-	}
-	private void cocosd4(){
-		player1 = CCSprite.sprite("player.png");
-		CGPoint initPos = CGPoint.ccp(100, 100);
-		player1.setPosition(initPos);
-		this.addChild(player1);
-		CGPoint tPos = CGPoint.ccp(300, 300);
-		CCMoveTo moveTo = CCMoveTo.action(2, tPos);
-		
-		CCRotateTo rotateTo = CCRotateTo.action(2, 180);
-		
-		CCScaleTo scaleTo = CCScaleTo.action(2, 2);
+    //    设置图片
+    private void cocosd10() {
+        player1 = CCSprite.sprite("plane.png");
+        CGPoint initPos = CGPoint.ccp(100, 100);
+        player1.setPosition(initPos);
+        this.addChild(player1);
+    }
+
+    private void cocosd9() {
+        this.schedule("func", 1);
+    }
+
+    public void func(float delta) {
+        Log.i(TAG, "func: " + delta);
+    }
+
+    private void cocosd8() {
+        this.setIsTouchEnabled(true);//设置可以点击
+    }
+
+    @Override
+    public boolean ccTouchesBegan(MotionEvent event) {//屏幕触摸事件
+        float x = event.getX();
+        float y = event.getY();
+        CGPoint p1 = CGPoint.ccp(x, y);
+        CCDirector director = CCDirector.sharedDirector();
+        CGPoint p2 = director.convertToGL(p1);
+        player1 = CCSprite.sprite("player.png");
+        player1.setPosition(p2);
+        this.addChild(player1);
+
+        return super.ccTouchesBegan(event);
+    }
+
+    @Override
+    public boolean ccTouchesEnded(MotionEvent event) {
+        this.removeChild(player1, true);
+        return super.ccTouchesEnded(event);
+    }
+
+    @Override
+    public boolean ccTouchesMoved(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        CGPoint p1 = CGPoint.ccp(x, y);
+        CCDirector director = CCDirector.sharedDirector();
+        CGPoint p2 = director.convertToGL(p1);
+        player1.setPosition(p2);
+        return super.ccTouchesMoved(event);
+    }
+
+    //    来回移动
+    private void cocosd7() {
+        player1 = CCSprite.sprite("player.png");
+        CGPoint initPos = CGPoint.ccp(100, 100);
+        player1.setPosition(initPos);
+        this.addChild(player1);
+        CGPoint tPos = CGPoint.ccp(300, 300);
+        CCMoveTo moveTo1 = CCMoveTo.action(2, tPos);
+        CCMoveTo moveTo2 = CCMoveTo.action(2, initPos);
+        CCSequence seq = CCSequence.actions(moveTo1, moveTo2);
+        CCRepeatForever repeat = CCRepeatForever.action(seq);
+        player1.runAction(repeat);
+    }
+
+    //    设置图片颜色ccColor3B
+    private void cocosd6() {
+        player1 = CCSprite.sprite("player.png");
+        CGPoint initPos = CGPoint.ccp(100, 100);
+        player1.setPosition(initPos);
+        this.addChild(player1);
+        ccColor3B cc = ccColor3B.ccc3(-120, 0, -255);
+        CCTintBy tintby = CCTintBy.action(3, cc);
+        player1.runAction(tintby);
+    }
+
+    //设置透明度切换
+    private void cocosd5() {
+        player1 = CCSprite.sprite("player.png");
+        CGPoint initPos = CGPoint.ccp(100, 100);
+        player1.setPosition(initPos);
+        this.addChild(player1);
+
+        CCFadeIn fadeIn = CCFadeIn.action(3);
+        CCFadeOut fadeOut = CCFadeOut.action(3);
+        CCSequence seq = CCSequence.actions(fadeIn, fadeOut);
+        player1.runAction(seq);
+    }
+
+    //    设置移动后停止
+    private void cocosd4() {
+        player1 = CCSprite.sprite("player.png");
+        CGPoint initPos = CGPoint.ccp(100, 100);
+        player1.setPosition(initPos);
+        this.addChild(player1);
+        CGPoint tPos = CGPoint.ccp(300, 300);
+        CCMoveTo moveTo = CCMoveTo.action(2, tPos);
+
+        CCRotateTo rotateTo = CCRotateTo.action(2, 180);
+
+        CCScaleTo scaleTo = CCScaleTo.action(2, 2);
 //		
 //		CCSequence seq = CCSequence.actions(moveTo, rotateTo,scaleTo);
 //		player1.runAction(seq);
 //		
 //		CCSpawn spawn = CCSpawn.actions(moveTo, rotateTo,scaleTo);
 //		player1.runAction(spawn);
-		
-		CCCallFuncN funcN = CCCallFuncN.action(this, "test1");
-		CCSequence seq = CCSequence.actions(moveTo,funcN);
-		player1.runAction(seq);
-	}
-	public void test1(Object sender) {
-		System.out.println("------------------");
-	}
-	private void cocosd3(){
-		player1 = CCSprite.sprite("player.png");
-		player2 = CCSprite.sprite("player.png");
-		this.addChild(player1);
-		this.addChild(player2);
-		
-		CGPoint initPos = CGPoint.ccp(400, 400);
-		player1.setPosition(initPos);
-		player2.setPosition(initPos);
-		
-		CGPoint dPos = CGPoint.ccp(0, 200);
-		CGPoint tPos = CGPoint.ccpAdd(initPos, dPos);
-		player2.setPosition(tPos);
-	}
-	
-	private void cocosd2(){
-		player1 = CCSprite.sprite("player.png");
-		CGPoint pos = CGPoint.ccp(100, 100);
-		player1.setPosition(pos);
+
+        CCCallFuncN funcN = CCCallFuncN.action(this, "test1");
+        CCSequence seq = CCSequence.actions(moveTo, funcN);
+        player1.runAction(seq);
+    }
+
+    public void test1(Object sender) {
+        System.out.println("------------------");
+    }
+
+    //    显示2张图片
+    private void cocosd3() {
+        player1 = CCSprite.sprite("player.png");
+        player2 = CCSprite.sprite("player.png");
+        this.addChild(player1);
+        this.addChild(player2);
+
+        CGPoint initPos = CGPoint.ccp(400, 400);
+        player1.setPosition(initPos);
+        player2.setPosition(initPos);
+
+        CGPoint dPos = CGPoint.ccp(0, 200);
+        CGPoint tPos = CGPoint.ccpAdd(initPos, dPos);
+        player2.setPosition(tPos);
+    }
+
+    //    图片自旋转
+    private void cocosd2() {
+        player1 = CCSprite.sprite("player.png");
+        CGPoint pos = CGPoint.ccp(100, 100);
+        player1.setPosition(pos);
 //		CCFlipY fy = CCFlipY.action(true);
 //		player.runAction(fy);
-		
+
 //		CGPoint tPos = CGPoint.ccp(400, 400);
 //		CCMoveTo toAction = CCMoveTo.action(3, tPos);
 //		player.runAction(toAction);
-		
-		CCRotateTo action3 = CCRotateTo.action(3, 180);
-		player1.runAction(action3);
-		this.addChild(player1);
-	}
+
+        CCRotateTo action3 = CCRotateTo.action(3, 180);
+        player1.runAction(action3);
+        this.addChild(player1);
+    }
 }
